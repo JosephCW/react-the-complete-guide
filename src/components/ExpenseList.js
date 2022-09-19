9a3cfc8eef27c283baab699182e97698ac74bc21
Joseph Watts <admin@josephcwatts.com>
2022-09-19 12:48:56 -0500

    Reduce duplicate code through props.children, wrapper component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,26 +1,38 @@
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 import './ExpenseList.css';
 
-const buildExpenseItemList = (expenses) => {
-  let expenseList = [];
-  expenses.forEach((expense, index) => {
-    expenseList.push(
-      <ExpenseItem
-        key={index}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    );
-  });
+// const buildExpenseItemList = (expenses) => {
+//   let expenseList = [];
+//   expenses.forEach((expense, index) => {
+//     expenseList.push(
+//       <ExpenseItem
+//         key={index}
+//         title={expense.title}
+//         amount={expense.amount}
+//         date={expense.date}
+//       />
+//     );
+//   });
 
-  return expenseList;
-};
+//   return expenseList;
+// };
 
 function ExpenseList(props) {
   return (
-    <div className="expense-list">{buildExpenseItemList(props.expenses)}</div>
+    <Card className="expense-list">
+      {props.expenses.map((expense, index) => {
+        return (
+          <ExpenseItem
+            key={index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+    </Card>
   );
 }
 
